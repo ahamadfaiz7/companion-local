d808f6f9cd5a1e449912010e470fe8f051b0e3ad
Mohammad Ahamad <mohammad.ahamad@paymentology.com>
2023-02-13 12:20:16 +0000

    Update Cardholder in Companion Local

--- src/main/java/com/companion/local/model/UpdateBearerRequest.java
+++ src/main/java/com/companion/local/model/UpdateBearerRequest.java
@@ -9,9 +9,13 @@ import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 @EqualsAndHashCode(callSuper=false)
 public class UpdateBearerRequest extends CreateLinkedCardRequest implements Serializable {
 
     private String cardIdentifier;
+
+    public UpdateBearerRequest(String reference, String firstName, String lastName, String idOrPassport, String cellphoneNumber, String expiryDate, String transactionId, String transactionDate, String cardIdentifier) {
+        super(reference, firstName, lastName, idOrPassport, cellphoneNumber, expiryDate, transactionId, transactionDate);
+        this.cardIdentifier = cardIdentifier;
+    }
 }
