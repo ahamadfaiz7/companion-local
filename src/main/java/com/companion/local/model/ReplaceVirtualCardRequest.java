87cc58c73b79a591e87f6efb38b75306830b524a
Mohammad Ahamad <mohammad.ahamad@paymentology.com>
2023-03-16 13:23:31 +0000

    adding gradle-wrapper.properties

---
+++ src/main/java/com/companion/local/model/ReplaceVirtualCardRequest.java
@@ -0,0 +1,25 @@
+package com.companion.local.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper=false)
+public class ReplaceVirtualCardRequest implements Serializable {
+
+    private String reference;
+    private String oldCardIdentifier;
+    private String firstName;
+    private String lastName;
+    private String idOrPassport;
+    private String cellphoneNumber;
+    private String expiryDate;
+    private String transactionId;
+    private String transactionDate;
+}
